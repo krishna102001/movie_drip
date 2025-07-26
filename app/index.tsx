58b1e58a503bb34f0daabef90b9a1d1467f5ea5a
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-26 15:44:25 +0530

    basic setup done

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className='text-amber-600 flex justify-center items-center'>
+        Hello Krishna
+      </Text>
+    </View>
+  );
+}
